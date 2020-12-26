f6b35c0f6e3dff8ddef35ff757278cc4ae3f3654
Lucas Vogel <contact@vogelino.com>
2020-12-26 23:34:58 +0100

    Fix some env. vars

--- utils/requestUtil.js
+++ utils/requestUtil.js
@@ -2,7 +2,7 @@ import { request as gqlRequest } from "graphql-request";
 
 export async function request(query, variables = {}, stateMapper = (x) => x) {
   const data = await gqlRequest(process.env.API_URL, query, {
-    stage: process.env.VERCEL_ENV !== "production" ? "DRAFT" : "PUBLISHED",
+    stage: process.env.NODE_ENV === "production" ? "PUBLISHED" : "DRAFT",
     ...variables,
   });
   return stateMapper(data);
