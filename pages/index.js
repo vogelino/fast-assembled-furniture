fad1469fb0ff549fc4b1c0dcb13860a1579a6c91
Lucas Vogel <contact@vogelino.com>
2020-12-26 16:31:27 +0100

    Add graphql example

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
 import Head from "next/head";
+import { request, gql } from "graphql-request";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ul>
+        {products.map(({ slug }) => (
+          <li key={slug}>{slug}</li>
+        ))}
+      </ul>
     </div>
   );
+}
+
+const query = gql`
+  {
+    products {
+      slug
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const data = await request("http://localhost:1337/graphql", query);
+  console.log(data);
+
+  return { props: data };
 }
