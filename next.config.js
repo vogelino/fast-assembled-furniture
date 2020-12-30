b81aebf8ffc1b57f66e3443a4584d14769a61d42
Lucas Vogel <contact@vogelino.com>
2020-12-30 23:29:00 +0100

    Include js files in functions

--- next.config.js
+++ next.config.js
@@ -3,5 +3,10 @@ const nextTranslate = require('next-translate')
 module.exports = nextTranslate({
   images: {
     domains: ['media.graphcms.com']
+  },
+  functions: {
+    'api/image.js': {
+      includeFiles: '**/*.js'
+    }
   }
 })
