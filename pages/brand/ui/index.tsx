e780a8958c6012f2cf49e9e655ee84dc5a459621
Lucas Vogel <contact@vogelino.com>
2021-01-27 22:14:59 +0100

    Add content for ui homepage in docs

--- pages/brand/ui/index.tsx
+++ pages/brand/ui/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import Layout from '@/brand/Layout'
+import text from '@/docs/brand/ui/index.md'
 
-const BrandGuide: FC = () => <Layout>UI library</Layout>
+const BrandGuide: FC = () => <Layout text={text}>UI library</Layout>
 
 export default BrandGuide
