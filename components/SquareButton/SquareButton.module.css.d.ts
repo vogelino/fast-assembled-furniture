e4dcbefdcdda1af347a8f06e19aaceeb28593261
Lucas Vogel <contact@vogelino.com>
2021-01-28 17:48:49 +0100

    Fix wrongly removed type definition

--- components/SquareButton/SquareButton.module.css.d.ts
+++ components/SquareButton/SquareButton.module.css.d.ts
@@ -1,3 +1,4 @@
 export const squareButton: string
 export const buttonContentContainer: string
 export const buttonContent: string
+export const textOnlyContainer: string
--- components/SquareButton/index.tsx
+++ components/SquareButton/index.tsx
@@ -1,6 +1,11 @@
 import { HTMLProps, FC } from 'react'
 import { icons, IconType } from '@/components/Icon'
-import { squareButton, buttonContent, buttonContentContainer } from './SquareButton.module.css'
+import {
+	squareButton,
+	buttonContent,
+	buttonContentContainer,
+	textOnlyContainer,
+} from './SquareButton.module.css'
 
 enum TypeColorMap {
 	Edit = '#FF9A6B',
