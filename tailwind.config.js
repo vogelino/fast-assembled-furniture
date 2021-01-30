103bf49ae673e79cdde539bd8471d812070f1c25
Lucas Vogel <contact@vogelino.com>
2021-01-30 03:02:57 +0100

    Use semmi color in buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,14 @@ module.exports = {
 	theme: {
 		colors: {
 			primary: 'var(--primary)',
+			primary50: 'var(--primary50)',
+			primary20: 'var(--primary20)',
 			secondary: 'var(--secondary)',
+			secondary50: 'var(--secondary50)',
+			secondary20: 'var(--secondary20)',
+			semi: 'var(--semi)',
+			semi50: 'var(--semi50)',
+			semi20: 'var(--semi20)',
 		},
 	},
 	variants: {
