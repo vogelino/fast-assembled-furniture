b72d0fac769b859c9142e991e51e336f5b64435c
Lucas Vogel <contact@vogelino.com>
2021-06-12 18:56:11 +0200

    fix(Sidebar): Fix docs sidebar link to Logo component

--- components/BrandGuide/Sidebar.tsx
+++ components/BrandGuide/Sidebar.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react'
-import { Logo } from '@components/Header/Logo'
+import { Logo } from '@components/Logo'
 import Menu, { PageTreeType } from '@components/BrandGuide/Menu'
 
 const SidebarHeader: FC = () => (
 	<div className="w-full-p">
 		<div className="w-full-p gf px-4 py-6">
-			<Logo framed={false} vertical={false} />
+			<Logo framed={false} />
 			<h1 className="w-full-p leading-6 mt-4 font-bold text-2xl uppercase">
 				Fast
 				<br />
