f6a209677ac5291756d2e3d8d947808ee1124fa4
Lucas Vogel <contact@vogelino.com>
2021-06-13 11:26:09 +0200

    feat(HomeMasonryGrid): Add mobile styles

--- components/HomeMasonryGrid/index.tsx
+++ components/HomeMasonryGrid/index.tsx
@@ -7,26 +7,34 @@ import styles from './HomeMasonryGrid.module.css'
 
 export const HomeMasonryGrid: FC = () => (
 	<div className={styles.container}>
-		<div>
+		<div className="relative">
 			<div className={[styles.buttonsContainer, 'inline-flex bg-primary'].join(' ')}>
 				<Button type="button" colorType="Buy">
-					Buy <span className="text-sm font-normal">(123€)</span>
+					Buy <span className="hidden sm:inline text-sm font-normal">(123€)</span>
 				</Button>
 				<ButtonWithBorderEdges
 					edges={[{ position: 'RightBottom', orientation: 'BottomLeft' }]}
 					openings={['TopRight']}
 					colorType="Edit"
 				>
-					Customize
+					Custom<span className="hidden sm:inline">ize</span>
 				</ButtonWithBorderEdges>
 			</div>
+			<BorderEdge
+				orientation="BottomRight"
+				className="opacity-100 sm:opacity-0 absolute bottom-0 right-0"
+			/>
+			<BorderEdge
+				orientation="BottomLeft"
+				className="opacity-100 sm:opacity-0 absolute -left-bd top-bd transform -translate-y-full"
+			/>
 		</div>
-		<div className="gfc -ml-bd -mt-bd w-full-p">
+		<div className="gfc -ml-bd -mt-bd w-full-p sm:rounded-tr-lg">
 			<div className="grid grid-cols-12 lg:grid-flow-col-dense">
-				<div className="gf col-span-6 lg:col-span-6 grid lg:grid-cols-5 p-8">
-					<div className="col-span-2">
+				<div className="gf col-span-12 sm:col-span-6 lg:col-span-6 grid lg:grid-cols-5 p-4 sm:p-8">
+					<div className="col-span-2 flex sm:block">
 						<Logo framed={false} className="transform scale-75 origin-top-left" />
-						<h4 className="text-2xl font-bold uppercase leading-6">
+						<h4 className="text-2xl font-bold uppercase leading-6 inline-block">
 							Fast
 							<br />
 							Assembled
@@ -34,22 +42,24 @@ export const HomeMasonryGrid: FC = () => (
 							Furniture
 						</h4>
 					</div>
-					<p className="md:text-xl lg:text-2xl col-span-3">
+					<p
+						className={['md:text-xl lg:text-2xl col-span-3 pl-12 sm:pl-0', '-mt-16 sm:mt-0'].join(
+							' '
+						)}
+					>
 						A customizable modular furniture system. Quick to assemble and disassemble. Industrial
 						components – or found objects – are collaged to form a piece of furniture.
 					</p>
 				</div>
 				<div
-					className={[styles.imageContainer, 'gf col-span-6 lg:col-span-6 lg:row-span-3'].join(' ')}
+					className={[
+						styles.imageContainer,
+						'gf col-span-12 sm:col-span-6 lg:col-span-6 lg:row-span-3 order-first sm:order-2',
+					].join(' ')}
 				>
 					<Image src="/images/LargeBlock01.png" width={696} height={808} objectFit="cover" />
 				</div>
-				<div
-					className={[
-						styles.imageContainer,
-						'gf lg:h-72 col-span-6 lg:col-span-3',
-					].join(' ')}
-				>
+				<div className={[styles.imageContainer, 'gf lg:h-72 col-span-6 lg:col-span-3'].join(' ')}>
 					<Image src="/images/Detail01.png" width={300} height={300} objectFit="cover" />
 				</div>
 				<div className="relative col-span-6 lg:col-span-3">
@@ -60,17 +70,17 @@ export const HomeMasonryGrid: FC = () => (
 				<div
 					className={[
 						styles.imageContainer,
-						'gf lg:h-72 col-span-6 lg:col-span-3',
+						'hidden sm:block gf lg:h-72 col-span-6 lg:col-span-3',
 					].join(' ')}
 				>
 					<Image src="/images/Detail02.png" width={300} height={300} objectFit="cover" />
 				</div>
-				<div className="relative col-span-6 lg:col-span-3">
-					<div className={[styles.middleBlock, 'gf h-full-p p-8 pt-24'].join(' ')}>
+				<div className="relative col-span-12 sm:col-span-6 lg:col-span-3">
+					<div className={[styles.middleBlock, 'gf h-full-p p-4 pt-16 sm:p-8 sm:pt-24'].join(' ')}>
 						<div
 							className={[
 								styles.marquee,
-								'absolute text-2xl uppercase font-bold whitespace-nowrap top-10',
+								'absolute text-2xl uppercase font-bold whitespace-nowrap top-6 sm:top-10',
 							].join(' ')}
 						>
 							<div className={styles.marqueeInner} aria-hidden>
