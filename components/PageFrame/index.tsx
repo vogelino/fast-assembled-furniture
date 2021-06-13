12c1bb15001c527f5d0b48b815d9076a5717bf35
Lucas Vogel <contact@vogelino.com>
2021-06-13 19:57:49 +0200

    refactor(PageFrame): Move border edges to its own component

---
+++ components/PageFrame/index.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react'
+import { BorderEdge } from '@components/BorderEdge'
+import styles from './PageFrame.module.css'
+
+export const PageFrame: FC = () => (
+	<div
+		className={[
+			styles.container,
+			'fixed pointer-events-none inset-0 page-edges z-50 rounded-lg border-bd',
+		].join(' ')}
+	>
+		<div className="relative w-full h-full">
+			<BorderEdge
+				orientation={'TopLeft' as const}
+				className="absolute pointer-events-none -top-bd -left-bd"
+			/>
+			<BorderEdge
+				orientation={'TopRight' as const}
+				className="absolute pointer-events-none -top-bd -right-bd"
+			/>
+			<BorderEdge
+				orientation={'BottomRight' as const}
+				className="absolute pointer-events-none -bottom-bd -right-bd"
+			/>
+			<BorderEdge
+				orientation={'BottomLeft' as const}
+				className="absolute pointer-events-none -bottom-bd -left-bd"
+			/>
+		</div>
+	</div>
+)
