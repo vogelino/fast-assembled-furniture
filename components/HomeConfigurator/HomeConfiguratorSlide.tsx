acdfd971c09c882083211d65e9107870705b3903
Lucas Vogel <contact@vogelino.com>
2021-08-14 11:13:22 +0200

    feat(Configurator): Better image loading

--- components/HomeConfigurator/HomeConfiguratorSlide.tsx
+++ components/HomeConfigurator/HomeConfiguratorSlide.tsx
@@ -1,8 +1,9 @@
-import { FC } from 'react'
+import { FC, useContext, useEffect, useState } from 'react'
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperClass from 'swiper/types/swiper-class'
 import styles from './HomeConfigurator.module.css'
+import { ColorThemeContext } from '@components/ColorThemeContext'
 
 interface SliderSlideType {
 	id: string
@@ -13,6 +14,49 @@ interface SliderImagePropType {
 	slides: SliderSlideType[]
 	onSlideChange: (currentSlide: number) => void
 	onReady: (swiper: SwiperClass) => void
+}
+
+const HomeConfiguratorSlideImage: FC<SliderSlideType> = ({ filePath }) => {
+	const { themeKey } = useContext(ColorThemeContext)
+	const [isLoaded, setIsLoaded] = useState(false)
+
+	useEffect(() => {
+		setIsLoaded(false)
+	}, [filePath])
+
+	return (
+		<>
+			<Image
+				src={filePath}
+				width={1536}
+				height={742}
+				objectFit="cover"
+				placeholder="blur"
+				blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP89B8AAukB8/71MdcAAAAASUVORK5CYII="
+				onLoad={() => setIsLoaded(true)}
+				className={['transition-all', isLoaded ? 'opacity-100' : 'opacity-60']
+					.filter(Boolean)
+					.join(' ')}
+			/>
+			<div
+				className={[
+					'absolute inset-0 grid place-content-center',
+					'transition-all',
+					isLoaded && 'opacity-0',
+				]
+					.filter(Boolean)
+					.join(' ')}
+				style={{ color: 'gray' }}
+			>
+				<div className="p-4 rounded relative bg-primary">
+					<div
+						className={styles.loadingSprite}
+						style={{ backgroundImage: `url("/images/configurator/LoadingSprite/${themeKey}.svg")` }}
+					></div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export const HomeConfiguratorSlide: FC<SliderImagePropType> = ({
@@ -28,16 +72,9 @@ export const HomeConfiguratorSlide: FC<SliderImagePropType> = ({
 			onSlideChange(swiper.activeIndex)
 		}}
 	>
-		{slides.map(({ id, filePath }) => (
-			<SwiperSlide key={id} className={styles.swiperSlide}>
-				<Image
-					src={filePath}
-					width={1536}
-					height={742}
-					objectFit="cover"
-					placeholder="blur"
-					blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP89B8AAukB8/71MdcAAAAASUVORK5CYII="
-				/>
+		{slides.map((slide) => (
+			<SwiperSlide key={slide.id} className={styles.swiperSlide}>
+				<HomeConfiguratorSlideImage {...slide} />
 			</SwiperSlide>
 		))}
 	</Swiper>
