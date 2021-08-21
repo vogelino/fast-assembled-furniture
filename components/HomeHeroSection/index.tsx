56f6f89b4791a0af31d5b1d1af941271fa66dd6f
Lucas Vogel <contact@vogelino.com>
2021-08-21 17:35:57 +0200

    fix(HomeHeroSection): Fix quality of hero image on mobile

--- components/HomeHeroSection/index.tsx
+++ components/HomeHeroSection/index.tsx
@@ -1,10 +1,22 @@
-import { LoadingImage } from '@components/LoadingImage'
+import useWindowSize from '@utils/hooks/useWindowSize'
+import { cover } from '@utils/intrinsicScale'
 import Image from 'next/image'
 import { FC, useState } from 'react'
 import styles from './HomeHeroSection.module.css'
 
+const imgWidth = 1920
+const imgHeight = 975
+
 export const HomeHeroSection: FC = () => {
 	const [imageIsLoaded, setImageIsloaded] = useState(false)
+	const windowSize = useWindowSize()
+	const zoomedInSize = cover({
+		parentHeight: windowSize.height - 60,
+		parentWidth: windowSize.width,
+		childWidth: imgWidth,
+		childHeight: imgHeight,
+	})
+	const isMobile = windowSize.width < 640
 
 	return (
 		<div
@@ -14,14 +26,18 @@ export const HomeHeroSection: FC = () => {
 				'w-full flex flex-wrap items-center',
 				'pointer-events-none',
 			].join(' ')}
+			style={{
+				['--imgWidth' as string]: zoomedInSize.width,
+				['--imgHeight' as string]: zoomedInSize.height,
+			}}
 		>
 			<div className="relative">
 				<Image
 					src="/images/HomeHeroImage.gif"
 					alt="An isometric view of the furniture"
-					width={1920}
-					height={975}
-					objectFit="cover"
+					width={isMobile ? zoomedInSize.width * 2 : imgWidth}
+					height={isMobile ? zoomedInSize.height * 2 : imgHeight}
+					layout={isMobile ? 'fixed' : 'intrinsic'}
 					quality={100}
 					className="opacity-10"
 				/>
@@ -30,12 +46,12 @@ export const HomeHeroSection: FC = () => {
 						imageIsLoaded ? 'opacity-100' : 'opacity-0'
 					} transition-opacity absolute inset-0`}
 				>
-					<LoadingImage
+					<Image
 						src="/images/HomeHeroImage.png"
 						alt="An isometric view of the furniture"
-						width={1920}
-						height={975}
-						objectFit="cover"
+						width={isMobile ? zoomedInSize.width * 2 : imgWidth}
+						height={isMobile ? zoomedInSize.height * 2 : imgHeight}
+						layout={isMobile ? 'fixed' : 'intrinsic'}
 						quality={100}
 						onLoad={() => {
 							setImageIsloaded(true)
